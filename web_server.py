3f0114e67bbc1eb42c0057180b2062db14eb3d8a
Chan Wai Yin <cwy6331@gmail.com>
2026-04-10 01:33:38 +0800

    update 2026

--- web_server.py
+++ web_server.py
@@ -3,6 +3,18 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/birthdaypage')
+def birthdaypage():
+    return render_template('birthdaypage.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -13,6 +25,10 @@ def cake():
 @app.route('/album')
 def album():
     return render_template('album.html')
+
+@app.route('/albumv2')
+def albumv2():
+    return render_template('albumv2.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
